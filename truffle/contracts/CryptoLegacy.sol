2557656cd78d5c0dd8ae18cdbd6f8b54b4a2c9cb
Sam Kozin <sam.kozin@gmail.com>
2017-10-13 21:36:28 +0300

    Implement `ownerCheckIn` API

--- truffle/contracts/CryptoLegacy.sol
+++ truffle/contracts/CryptoLegacy.sol
@@ -1,5 +1,7 @@
 pragma solidity ^0.4.12;
 
+import "SafeMath.sol";
+
 contract CryptoLegacyContract {
 
   event DebugEvent(string debug);
@@ -45,9 +47,16 @@ contract CryptoLegacyContract {
   }
 
   address public owner = msg.sender;
+
   uint public checkInInterval;
   uint public keepingFee;
   uint public finalReward;
+
+  // We can rely on the value of now (block.timestamp) for our purposes, as the consensus
+  // rule is that a block's timestamp must be 1) more than the parent's block timestamp;
+  // and 2) less than the current wall clock time. See:
+  // https://github.com/ethereum/go-ethereum/blob/885c13c/consensus/ethash/consensus.go#L223
+  uint public lastOwnerCheckInAt = now;
 
   States public state = States.CallForKeepers;
 
@@ -59,8 +68,16 @@ contract CryptoLegacyContract {
   mapping(address => ActiveKeeper) public activeKeepers;
   address[] public activeKeepersAddresses; // TODO: make internal
 
+  uint totalFinalReward;
 
-  // Called by Alice.
+  // We need this because current version of Solidity doesn't support non-integer numbers.
+  // We set it to be equal to number of wei in eth to make sure we transfer keeping fee with
+  // enough precision.
+  uint constant KEEPING_FEE_PRECISION = 1 ether;
+
+
+  // Called by the person who possesses the data they wish to transfer.
+  // This person becomes the owner of the contract.
   //
   function CryptoLegacyContract(uint _checkInInterval, uint _keepingFee, uint _finalReward) public {
     checkInInterval = _checkInInterval;
@@ -86,7 +103,7 @@ contract CryptoLegacyContract {
   }
 
 
-  // Called by Alice to accept selected proposals and activate the contract.
+  // Called by owner to accept selected proposals and activate the contract.
   //
   function acceptKeepers(
     uint[] selectedProposalIndices,
@@ -94,8 +111,11 @@ contract CryptoLegacyContract {
     bytes encryptedKeyParts,
     bytes encryptedData,
     bytes32 dataHash) external
-    atState(States.CallForKeepers) ownerOnly()
+    ownerOnly()
+    atState(States.CallForKeepers)
   {
+    uint _totalFinalReward = 0;
+
     encyptedData = EncyptedData({
       encryptedData: encryptedData,
       dataHash: dataHash,
@@ -114,17 +134,51 @@ contract CryptoLegacyContract {
       });
 
       activeKeepersAddresses.push(proposal.keeperAddress);
+
+      uint keeperFinalReward = finalReward;
+      _totalFinalReward = SafeMath.add(_totalFinalReward, keeperFinalReward);
     }
 
+    require(this.balance >= _totalFinalReward);
+
     state = States.Active;
+    totalFinalReward = _totalFinalReward;
+    lastOwnerCheckInAt = now;
   }
 
 
+  // Updates owner check-in time and credits all active Keepers with keeping fee.
+  //
   function ownerCheckIn() payable external
     ownerOnly()
     atState(States.Active)
   {
-    // TODO: implement
+    uint timeSinceLastOwnerCheckIn = SafeMath.sub(now, lastOwnerCheckInAt);
+
+    if (timeSinceLastOwnerCheckIn > checkInInterval) {
+      revert();
+    }
+
+    uint keepingFeeMult = SafeMath.mul(KEEPING_FEE_PRECISION, timeSinceLastOwnerCheckIn) / checkInInterval;
+    uint keepersBalance = 0;
+
+    for (uint i = 0; i < activeKeepersAddresses.length; i++) {
+      ActiveKeeper keeper = activeKeepers[activeKeepersAddresses[i]];
+      uint balanceToAdd = SafeMath.mul(keepingFee, keepingFeeMult) / KEEPING_FEE_PRECISION;
+      keeper.balance = SafeMath.add(keeper.balance, balanceToAdd);
+      keepersBalance = SafeMath.add(keepersBalance, keeper.balance);
+    }
+
+    require(this.balance >= SafeMath.add(keepersBalance, totalFinalReward));
+
+    lastOwnerCheckInAt = now;
+  }
+
+
+  // TODO: remove (for debug purposes only)
+  //
+  function forceOwnerCheckIn() external {
+    lastOwnerCheckInAt = now;
   }
 
 
--- truffle/migrations/2_deploy_contracts.js
+++ truffle/migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 var CryptoLegacy = artifacts.require("./CryptoLegacy.sol");
+var SafeMath = artifacts.require("./SafeMath.sol");
 
 module.exports = function(deployer) {
+  deployer.link(SafeMath, CryptoLegacy);
   deployer.deploy(CryptoLegacy);
 };
