e0d6121b82e0811016fc33fcc92f5eb1b62a06a1
Sam Kozin <sam.kozin@gmail.com>
2017-10-19 20:12:21 +0300

    Add test blockchain; unlock test accounts when starting geth

--- truffle/run-geth.sh
+++ truffle/run-geth.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-exec geth --dev \
+rm -rf .blockchain
+tar -xzf blockchain.tar.gz
+
+geth --dev \
+  --datadir .blockchain \
   --rpc --rpcapi="db,eth,net,web3,personal" \
   --rpcport "8545" --rpcaddr "127.0.0.1" --rpccorsdomain "localhost" \
+  --unlock '0,1,2,3,4,5,6,7,8' \
+  --password geth-passwords.txt \
+  --mine \
   console
