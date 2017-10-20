d6c28407518ec89aecf2672d67cd33d414fce6b8
Sam Kozin <sam.kozin@gmail.com>
2017-10-20 17:01:38 +0300

    Make geth nicer

--- truffle/run-geth.sh
+++ truffle/run-geth.sh
@@ -3,7 +3,7 @@
 rm -rf .blockchain
 tar -xzf blockchain.tar.gz
 
-geth --dev \
+nice -n 5 geth --dev \
   --datadir .blockchain \
   --rpc --rpcapi="db,eth,net,web3,personal" \
   --rpcport "8545" --rpcaddr "127.0.0.1" --rpccorsdomain "localhost" \
