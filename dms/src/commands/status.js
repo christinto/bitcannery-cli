2358755ff0cb9fe68df7820a8b48a625fa9f8e62
Sam Kozin <sam.kozin@gmail.com>
2017-12-13 18:39:24 +0300

    Improve status command

--- dms/src/commands/status.js
+++ dms/src/commands/status.js
@@ -1,7 +1,7 @@
 import moment from 'moment'
 import getWeb3 from '../utils/get-web3'
 import getContractClass from '../utils/get-contract-class'
-const {stateToString} = require('../utils/contract-api')
+const {States, stateToString} = require('../utils/contract-api')
 
 export const description = 'Display the status of given legacy contract'
 
@@ -21,31 +21,44 @@ export async function handler(argv) {
   const LegacyContract = await getContractClass()
   const instance = await LegacyContract.at(argv.contract_id)
 
-  const owner = await instance.owner()
-  const state = await instance.state()
-  const checkInIntervalInSec = (await instance.checkInInterval()).toNumber()
-  const numKeepers = (await instance.getNumKeepers()).toNumber()
-  const lastOwnerCheckInAt = (await instance.lastOwnerCheckInAt()).toNumber()
-  const totalKeepingFee = await instance.totalKeepingFee()
-
-  const totalKeepingFeeEth = getWeb3().fromWei(totalKeepingFee, 'ether')
+  const [owner, state, checkInIntervalInSec, lastOwnerCheckInAt] = [
+    await instance.owner(),
+    (await instance.state()).toNumber(),
+    (await instance.checkInInterval()).toNumber(),
+    (await instance.lastOwnerCheckInAt()).toNumber(),
+  ]
 
   console.log()
   console.log('Contract address: ', argv.contract_id)
-  console.log('Owner: account', owner)
-  console.log('Contract state:', stateToString(state.toNumber()))
-  console.log('Number of keepers:', numKeepers)
+  console.log('Owner:', owner)
+  console.log('Contract state:', stateToString(state))
+
+  if (state === States.CallForKeepers) {
+    const numProposals = await instance.getNumProposals()
+    console.log(`Number of keepeing proposals: ${numProposals}`)
+  } else {
+    const [numKeepers, totalKeepingFee] = [
+      await instance.getNumKeepers(),
+      await instance.totalKeepingFee(),
+    ]
+    const totalKeepingFeeEth = getWeb3().fromWei(totalKeepingFee, 'ether')
+    console.log(`Number of keepers: ${numKeepers}`)
+    console.log(`Combined keepers fee: ${totalKeepingFeeEth} ETH`)
+  }
+
   console.log(
-    'Check in intreval:',
+    'Check-in intreval:',
     moment()
       .add(checkInIntervalInSec, 's')
       .toNow(true),
   )
-  console.log('Combined keepers fee:', totalKeepingFeeEth.toString(), 'ETH')
-  console.log(
-    'The next check-in:',
-    moment(lastOwnerCheckInAt * 1000)
-      .add(checkInIntervalInSec, 's')
-      .fromNow(),
-  )
+
+  if (state === States.Active) {
+    console.log(
+      'The next check-in:',
+      moment(lastOwnerCheckInAt * 1000)
+        .add(checkInIntervalInSec, 's')
+        .fromNow(),
+    )
+  }
 }
