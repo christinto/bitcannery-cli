ba9a01f1913c4018bf73ad6773ab287e09709f41
Sam Kozin <sam.kozin@gmail.com>
2017-12-14 17:05:35 +0300

    Fix currency formatting

--- dms/src/utils/format.js
+++ dms/src/utils/format.js
@@ -2,9 +2,12 @@ import BigNumber from 'bignumber.js'
 
 export function formatWei(weiAmount) {
   weiAmount = new BigNumber(weiAmount)
-  if (weiAmount.greaterThanOrEqualTo('1e9')) {
+  const weiAmountAbs = weiAmount.abs()
+  if (weiAmountAbs.greaterThanOrEqualTo('1e12')) {
     return weiAmount.shift(-18) + ' ETH'
-  } else {
+  } else if (weiAmountAbs.greaterThanOrEqualTo('1e7')) {
     return weiAmount.shift(-9) + ' Gwei'
+  } else {
+    return weiAmount + ' wei'
   }
 }
