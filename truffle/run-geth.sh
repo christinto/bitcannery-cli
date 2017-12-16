96ef50ceb70fd2d707f29e87e23ef1c3cee0928c
ujenjt <pshenichniy.eugene@gmail.com>
2017-12-16 17:07:43 +0300

    Add CORS to allow connection from remix IDE

--- truffle/run-geth.sh
+++ truffle/run-geth.sh
@@ -6,7 +6,7 @@ tar -xzf blockchain.tar.gz
 nice -n 5 geth --dev \
   --datadir .blockchain \
   --rpc --rpcapi="db,eth,net,web3,personal" \
-  --rpcport "8545" --rpcaddr "127.0.0.1" --rpccorsdomain "localhost" \
+  --rpcport "8545" --rpcaddr "127.0.0.1" --rpccorsdomain "*" \
   --unlock '0,1,2,3,4,5,6,7,8' \
   --password geth-passwords.txt \
   --mine \
