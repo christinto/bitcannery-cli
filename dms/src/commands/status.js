97246ec1b5588ec0b32ff673c1ae329466074e8c
ujenjt <pshenichniy.eugene@gmail.com>
2018-02-22 18:33:00 +0300

    Add displaying of contract chain for status cmd

--- dms/src/commands/status.js
+++ dms/src/commands/status.js
@@ -13,7 +13,7 @@ export const builder = yargs => yargs
 import moment from 'moment'
 import getWeb3 from '../utils/get-web3'
 import getContractInstance from '../utils/get-contract-instance'
-import {States} from '../utils/contract-api'
+import {States, fetchContractChain} from '../utils/contract-api'
 import {formatWei} from '../utils/format'
 import runCommand from '../utils/run-command'
 
@@ -65,4 +65,24 @@ async function getStatus(contractAddressOrID) {
         .fromNow(),
     )
   }
+
+  if (contractAddressOrID.substring(0, 2) != '0x') {
+    const chain = await fetchContractChain(contractAddressOrID)
+    console.error(`\nPrevious contracts in chain [${chain.length - 1}]\n`)
+    chain
+      .reverse()
+      .slice(1)
+      .forEach(displayShortInfo)
+  }
+}
+
+async function displayShortInfo(contract) {
+  const [state, lastOwnerCheckInAt] = [
+    (await contract.state()).toNumber(),
+    (await contract.lastOwnerCheckInAt()).toNumber(),
+  ]
+  console.error('Contract address:', contract.address)
+  console.error('Contract state:', States.stringify(state))
+  console.error('Last owner checkin:', moment(lastOwnerCheckInAt * 1000).format('DD MMM YYYY'))
+  console.error()
 }
