f426611d0cb1e2ce60b0e8606f27ef29ab0d4a77
ujenjt <pshenichniy.eugene@gmail.com>
2018-03-08 20:10:08 +0300

    Meet geth 1.8.* deprication warning about account unlocking

--- rinkeby/run-geth.sh
+++ rinkeby/run-geth.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
 set -e
 
-UNLOCK='0,1,2,3'
+function join {
+  local IFS="$1";
+  shift;
+  echo "$*";
+}
+
+ACC=(
+  '0x6da26a02b4364dcff7cfd58f8a8b9c6ce62a0c61'
+  '0xbb2bced367d8c4712baac44616c1e61797f392a3'
+  '0xc712deae0ab6abf65285ed42400b127056f3c664'
+  '0x80433df99abe278680a20f0bc70bbf243d51c803'
+)
+ACCOUNTS_TO_UNLOCK=$(join , ${ACC[@]})
 DATADIR="~/.rinkeby"
 
 geth \
@@ -9,6 +21,6 @@ geth \
   --rinkeby \
   --rpc --rpcapi='db,eth,net,web3,personal' \
   --rpcport '9545' --rpcaddr '127.0.0.1' --rpccorsdomain '*' \
-  --unlock "$UNLOCK" \
   --password rinkeby-passwords.txt \
+  --unlock "$ACCOUNTS_TO_UNLOCK" \
   console
