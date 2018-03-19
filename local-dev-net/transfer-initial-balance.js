523a592351f58e212ece4fc3eab1f50d68ceac96
Sam Kozin <sam.kozin@gmail.com>
2018-03-19 21:44:35 +0300

    Add more accounts to local dev net

--- local-dev-net/transfer-initial-balance.js
+++ local-dev-net/transfer-initial-balance.js
@@ -1,4 +1,5 @@
 
 for (var i = 1; i < accounts.length; ++i) {
-  eth.sendTransaction({from: eth.accounts[0], to: eth.accounts[i], value: '1000000000000000000000'});
+  eth.sendTransaction({from: eth.accounts[0], to: eth.accounts[i], value: '1000000000000000000'});
+  console.log("Transferred initial balance to account " + i);
 }
