0a67e45a8645e76b6cee604e32cba8c92e958d9b
ujenjt <pshenichniy.eugene@gmail.com>
2018-04-16 17:36:20 +0300

    Add displaying of the balance

--- dms/src/contract-utils/common.js
+++ dms/src/contract-utils/common.js
@@ -1,8 +1,13 @@
+import BigNumber from 'bignumber.js'
 import print from '../utils/print'
 
-import {getNetworkName} from '../utils/web3'
+import {getNetworkName, getBalance} from '../utils/web3'
+import {formatWei} from '../utils/format'
 import getPreparedAccount from '../utils/accounts/get-prepared-account'
 import getContractAPIs from '../utils/get-contract-apis'
+
+const ACCOUNT_LOW_BALANCE = new BigNumber('5e17') // 0.5 ETH
+const ACCOUNT_CRITICAL_LOW_BALANCE = new BigNumber('1e15') // 0.001 ETH
 
 export async function printWelcomeAndUnlockAccount() {
   print('Welcome to KeeperNet v2!\n')
@@ -10,10 +15,22 @@ export async function printWelcomeAndUnlockAccount() {
   const networkName = await getNetworkName()
   const address = await getPreparedAccount()
   const {registry} = await getContractAPIs()
+  const balance = await getBalance(address)
 
   print(`Your address: ${address}`)
   print(`Network: ${networkName}`)
-  print(`Registry: ${registry.address}\n`)
+  print(`Registry: ${registry.address}`)
+  print(`Wallet balance: ${formatWei(balance)}\n`)
+
+  if (balance.lt(ACCOUNT_CRITICAL_LOW_BALANCE)) {
+    print('Account balance is too low, please add some ether on it.\n')
+    process.exit(1)
+    return
+  }
+
+  if (balance.lt(ACCOUNT_LOW_BALANCE)) {
+    print('Warning: Account balance is low, please refill it shortly.\n')
+  }
 
   return address
 }
